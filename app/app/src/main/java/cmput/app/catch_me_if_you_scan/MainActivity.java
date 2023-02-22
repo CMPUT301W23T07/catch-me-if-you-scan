80846b159c078b8ffef9ee41b043b290992ce251
Riley Zilka <rzilka@rodeoready.com>
2023-02-22 11:17:32 -0700

    app created

---
+++ app/app/src/main/java/cmput/app/catch_me_if_you_scan/MainActivity.java
@@ -0,0 +1,14 @@
+package cmput.app.catch_me_if_you_scan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
