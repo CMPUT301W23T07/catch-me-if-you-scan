927e2cb9f61c2dad8bfcadcc9074f9099d965849
Moh Ahmadi <mohammad.ahmadi@rodeoready.com>
2023-02-23 16:54:31 -0700

    started User code

---
+++ app/app/src/main/java/cmput/app/catch_me_if_you_scan/Monster.java
@@ -0,0 +1,9 @@
+package cmput.app.catch_me_if_you_scan;
+
+public class Monster {
+    private int score;
+
+    public int getScore() {
+        return this.score;
+    }
+}
---
+++ app/app/src/main/java/cmput/app/catch_me_if_you_scan/User.java
@@ -0,0 +1,62 @@
+package cmput.app.catch_me_if_you_scan;
+
+import android.graphics.Region;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class User {
+
+    private String name;
+    private String email;
+    private int score;
+//    private Region region;
+    private PriorityQueue<Monster> monsters = new PriorityQueue<Monster>(11, new Comparator<Monster>() {
+    @Override
+    public int compare(Monster t1, Monster t2) {
+        if (t1.getScore() > t2.getScore()) {
+            return 1;
+        }
+        return -1;
+    };
+    });
+
+    // Constructor
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // getters
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+//    public Location getLocation() {
+//        return this.location;
+//    }
+
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void addMonster(Monster monster) {
+        monsters.add(monster);
+        this.score += monster.getScore();
+    }
+
+
+}
