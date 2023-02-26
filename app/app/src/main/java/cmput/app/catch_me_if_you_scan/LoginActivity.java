64d05c0881dc321edd3e2d04a10668c31dbca7ce
Riley Zilka <rileyzilka33@gmail.com>
2023-02-25 17:44:58 -0700

    abc

---
+++ app/app/src/main/java/cmput/app/catch_me_if_you_scan/LoginActivity.java
@@ -0,0 +1,13 @@
+package cmput.app.catch_me_if_you_scan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
