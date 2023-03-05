16ea4dce59ed5de87e713c3ab42e31fa682f4fca
Riley Zilka <rileyzilka33@gmail.com>
2023-03-05 14:50:27 -0700

    more map setup

--- app/app/src/main/java/cmput/app/catch_me_if_you_scan/LoginActivity.java
+++ app/app/src/main/java/cmput/app/catch_me_if_you_scan/LoginActivity.java
@@ -3,17 +3,29 @@ package cmput.app.catch_me_if_you_scan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        isServicesOK();
 
         Button signUp = findViewById(R.id.sign_up_button);
 
@@ -25,5 +37,28 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    public void isServicesOK() {
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+
+        if (available == ConnectionResult.SUCCESS) {
+            //Maps connection works
+            Log.d(TAG, "isServicesOK: Google play service is working");
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            //Error occurred but it can be resolved easily
+            Log.d(TAG, "isServicesOK: the version error can be resolved");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(this, available, ERROR_DIALOG_REQUEST);
+            if (dialog != null) {
+                dialog.show();
+            }
+        }
+        else {
+            Toast.makeText(this, "You can't make map requests, API cannot connect.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
