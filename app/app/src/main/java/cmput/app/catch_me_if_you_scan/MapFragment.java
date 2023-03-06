f0c5a30db578c1756be8de523d187590f6c8ad0f
Riley Zilka <rileyzilka33@gmail.com>
2023-03-05 18:06:56 -0700

    Map now loads and displays current location

--- app/app/src/main/java/cmput/app/catch_me_if_you_scan/MapFragment.java
+++ app/app/src/main/java/cmput/app/catch_me_if_you_scan/MapFragment.java
@@ -1,34 +1,34 @@
 package cmput.app.catch_me_if_you_scan;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     MapView mView;
     GoogleMap map;
-
     PermissionManager permissions;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    Location userLocation;
+    FusedLocationProviderClient mLocationClient;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -52,8 +52,57 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(@NonNull GoogleMap googleMap) {
         map = googleMap;
         map.getUiSettings().setMyLocationButtonEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(53.5461, 113.4937)));
         if (permissions.hasLocationPermissions()) {
             map.setMyLocationEnabled(true);
+            getCurrentLocation();
         }
+    }
+
+    //Permissions are checked with the line "if (permissions.hasLocationPermissions())"
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation() {
+
+        mLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        Task<Location> location = mLocationClient.getLastLocation();
+        location.addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if (task.isSuccessful()) {
+                    userLocation = (Location) task.getResult();
+                    if (userLocation != null) {
+                        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(userLocation.getLatitude(), userLocation.getLongitude())));
+                    }
+                    else {
+                        Toast.makeText(getActivity(), "Unable to get current location", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mView.onLowMemory();
     }
 }
