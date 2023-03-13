635e5d7d53c1fcb5eab3c78981755f1d19bea6c6
MarkJuliusAbraham <markjuli@ualberta.ca>
2023-03-13 13:14:34 -0600

    added Javadocs to multiple tests

--- app/app/src/androidTest/java/cmput/app/catch_me_if_you_scan/PermissionDialogTest.java
+++ app/app/src/androidTest/java/cmput/app/catch_me_if_you_scan/PermissionDialogTest.java
@@ -18,28 +18,5 @@ import android.app.Dialog;
 import android.widget.TextView;
 
 public class PermissionDialogTest {
-
-//    @Test
-//    public void testDialogIsDisplayed() {
-//        // Launch the activity
-//        ActivityScenario<MainActivity> activityScenario = ActivityScenario.launch(MainActivity.class);
-//
-//        // Wait for the activity to be launched and displayed
-//        activityScenario.onActivity(activity -> {
-//            // Launch the fragment in the activity's container
-//            FragmentScenario<PermissionDialog> fragmentScenario = launchInContainer(PermissionDialog.class, null);
-//
-//            // Wait for the fragment to be launched and displayed
-//            fragmentScenario.onFragment(fragment -> {
-//                // Verify that the dialog is not null
-//                Dialog dialog = fragment.getDialog();
-//                assertNotNull(dialog);
-//
-//                // Verify that the message in the dialog is correct
-//                String expectedMessage = "This app requires the storage, camera and location permission.\n\nPlease manually grant the permission in the app settings.";
-//                String actualMessage = ((TextView) dialog.findViewById(android.R.id.message)).getText().toString();
-//                assertEquals(expectedMessage, actualMessage);
-//            });
-//        });
-//    }
+    //TO DO: Add tests
 }
